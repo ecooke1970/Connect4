ca293b673ed5b3a6b90ceaf34a7900d6e86af366
Erik <46272257+ecooke1970@users.noreply.github.com>
2021-01-28 22:20:56 -0500

    Lots of GUI practice
    Messing around with GUI

--- Connect4.py
+++ Connect4.py
@@ -5,52 +5,6 @@ from tkinter import *
 import tkinter as tk
 
 
-def draw_board(self):
-    window = tk.Tk()
-    window.title("Connect 4")
-
-    top_frame = tk.Frame(window)
-    my_btn1 = tk.Button(top_frame, text="C1", command=player_move(self, 1))
-    my_btn2 = tk.Button(top_frame, text="C2")
-    my_btn3 = tk.Button(top_frame, text="C3")
-    my_btn4 = tk.Button(top_frame, text="C4")
-    my_btn5 = tk.Button(top_frame, text="C5")
-    my_btn6 = tk.Button(top_frame, text="C6")
-    my_btn7 = tk.Button(top_frame, text="C7")
-    top_frame.pack()
-    my_btn1.grid(row=0, column=0)
-    my_btn2.grid(row=0, column=1)
-    my_btn3.grid(row=0, column=2)
-    my_btn4.grid(row=0, column=3)
-    my_btn5.grid(row=0, column=4)
-    my_btn6.grid(row=0, column=5)
-    my_btn7.grid(row=0, column=6)
-
-    bottom_frame = tk.Frame(window)
-    my_canvas = tk.Canvas(bottom_frame, bg="blue", height=700, width=800)
-    # my_canvas.create_oval(500, 500, 550, 550, fill="red")
-
-    x = 0
-    y = 0
-    for i in range(6):
-        row = self.board.get_row(i)
-        for col in row:
-            if col == ' ':
-                my_canvas.create_oval(100 * x, 100 * y, x * 100 + 100, y * 100 + 100, fill="black")
-            elif col == 'X':
-                my_canvas.create_oval(100 * x, 100 * y, x * 100 + 100, y * 100 + 100, fill="yellow")
-            else:
-                my_canvas.create_oval(100 * x, 100 * y, x * 100 + 100, y * 100 + 100, fill="red")
-            x += 1
-        y += 1
-        x = 0
-
-    bottom_frame.pack(side="bottom")
-    my_canvas.pack()
-
-    window.mainloop()
-
-
 class Connect4:
     def __init__(self):
         self.board = GameBoard(6, 7)
@@ -59,121 +13,127 @@ class Connect4:
         self.winner = False
 
     def play(self):
+        print("play")
+
+        def player_move(column):
+            print("player_move executed")
+            column -= 1
+
+            valid_move = False
+            place_found = False
+            i = 5
+            while not place_found:
+                if i < 0:
+                    place_found = True
+                elif self.board.get_position(i, column) == ' ':
+                    place_found = True
+                    valid_move = True
+                else:
+                    i -= 1
+
+            if valid_move:
+                self.moves += 1
+                self.board.set_position(self.player, i, column)
+                if self.moves > 6:
+                    self.winner = check_for_winner()
+
+                # if no winner, check if board is full, if not switch players
+                if not self.winner:
+                    if self.moves > 41:
+                        self.player = 'T'
+                        self.winner = True
+                    elif self.player == 'X':
+                        self.player = 'Y'
+                    else:
+                        self.player = 'X'
+                self.board.print_board()
+                window.update()
+
+        def check_for_winner():
+            print("check_for_winner")
+            # Check horizontally by row
+            for i in range(self.board.get_num_rows()):
+                if (self.player * 4) in self.board.get_row(i):
+                    return True
+
+            # Check vertically by column
+            for i in range(self.board.get_num_columns()):
+                if (self.player * 4) in self.board.get_column(i):
+                    return True
+
+            # Check diagonally every row first column up right and down right
+            for i in range(self.board.get_num_rows()):
+                if (self.player * 4) in self.board.get_diagonal(row=i, column=0, direction="dr"):
+                    return True
+                if (self.player * 4) in self.board.get_diagonal(row=i, column=0, direction="ur"):
+                    return True
+
+            # Check diagonally every column first row down right and every column last row up right
+            for i in range(self.board.get_num_columns()):
+                if (self.player * 4) in self.board.get_diagonal(row=0, column=i, direction="dr"):
+                    return True
+                if (self.player * 4) in self.board.get_diagonal(row=self.board.get_num_rows() - 1, column=i,
+                                                                direction="ur"):
+                    return True
+
+            return False
+
         print("\nWelcome to Connect 4")
 
-        # self.board.set_position('X', 2, 2)
-        # self.board.set_position('Y', 3, 2)
-        # self.board.set_position('X', 2, 6)
-        # self.board.set_position('Y', 5, 2)
+        self.board.set_position('X', 2, 2)
+        self.board.set_position('Y', 3, 2)
+        self.board.set_position('X', 2, 6)
+        self.board.set_position('Y', 5, 2)
 
-        while not self.winner:
-            # self.board.print_board()
-            window = tk.Tk()
-            window.title("Connect 4")
+        # while not self.winner:
 
-            top_frame = tk.Frame(window)
-            my_btn1 = Button(top_frame, text="C1", command=Connect4.player_move(self, 1))
-            my_btn2 = Button(top_frame, text="C2")
-            my_btn3 = Button(top_frame, text="C3")
-            my_btn4 = Button(top_frame, text="C4")
-            my_btn5 = Button(top_frame, text="C5")
-            my_btn6 = Button(top_frame, text="C6")
-            my_btn7 = Button(top_frame, text="C7")
-            top_frame.pack()
-            my_btn1.grid(row=0, column=0)
-            my_btn2.grid(row=0, column=1)
-            my_btn3.grid(row=0, column=2)
-            my_btn4.grid(row=0, column=3)
-            my_btn5.grid(row=0, column=4)
-            my_btn6.grid(row=0, column=5)
-            my_btn7.grid(row=0, column=6)
+        window = tk.Tk()
+        window.title("Connect 4")
 
-            bottom_frame = tk.Frame(window)
-            my_canvas = tk.Canvas(bottom_frame, bg="blue", height=700, width=800)
-            # my_canvas.create_oval(500, 500, 550, 550, fill="red")
+        top_frame = tk.Frame(window)
+        my_btn1 = Button(top_frame, text="C1", command=lambda: player_move(1))
+        my_btn2 = Button(top_frame, text="C2", command=lambda: player_move(2))
+        my_btn3 = Button(top_frame, text="C3", command=lambda: player_move(3))
+        my_btn4 = Button(top_frame, text="C4", command=lambda: player_move(4))
+        my_btn5 = Button(top_frame, text="C5", command=lambda: player_move(5))
+        my_btn6 = Button(top_frame, text="C6", command=lambda: player_move(6))
+        my_btn7 = Button(top_frame, text="C7", command=lambda: player_move(7))
+        top_frame.pack()
+        my_btn1.grid(row=0, column=0)
+        my_btn2.grid(row=0, column=1)
+        my_btn3.grid(row=0, column=2)
+        my_btn4.grid(row=0, column=3)
+        my_btn5.grid(row=0, column=4)
+        my_btn6.grid(row=0, column=5)
+        my_btn7.grid(row=0, column=6)
 
-            x = 0
-            y = 0
-            for i in range(6):
-                row = self.board.get_row(i)
-                for col in row:
-                    if col == ' ':
-                        my_canvas.create_oval(100 * x, 100 * y, x * 100 + 100, y * 100 + 100, fill="black")
-                    elif col == 'X':
-                        my_canvas.create_oval(100 * x, 100 * y, x * 100 + 100, y * 100 + 100, fill="yellow")
-                    else:
-                        my_canvas.create_oval(100 * x, 100 * y, x * 100 + 100, y * 100 + 100, fill="red")
-                    x += 1
-                y += 1
-                x = 0
+        bottom_frame = tk.Frame(window)
+        my_canvas = tk.Canvas(bottom_frame, bg="blue", height=700, width=800)
 
-            bottom_frame.pack(side="bottom")
-            my_canvas.pack()
-
-            window.mainloop()
-
-        print("Game Over")
-        if self.player == 'T':
-            print("The game is a tie")
-        elif self.player == "X":
-            print("The winner is player 1")
-        else:
-            print("The winner is player 2")
-
-    def player_move(self, column):
-        column -= 1
-
-        valid_move = False
-        place_found = False
-        i = 5
-        while not place_found:
-            if i < 0:
-                place_found = True
-            elif self.board.get_position(i, column) == ' ':
-                place_found = True
-                valid_move = True
-            else:
-                i -= 1
-
-        if valid_move:
-            self.moves += 1
-            self.board.set_position(self.player, i, column)
-            if self.moves > 6:
-                self.winner = Connect4.check_for_winner(self)
-
-            # if no winner, check if board is full, if not switch players
-            if not self.winner:
-                if self.moves > 41:
-                    self.player = 'T'
-                    self.winner = True
-                elif self.player == 'X':
-                    self.player = 'Y'
+        x = 0
+        y = 0
+        for i in range(6):
+            row = self.board.get_row(i, return_form="list")
+            for col in row:
+                if col == ' ':
+                    my_canvas.create_oval(100 * x, 100 * y, x * 100 + 100, y * 100 + 100, fill="black")
+                elif col == 'X':
+                    my_canvas.create_oval(100 * x, 100 * y, x * 100 + 100, y * 100 + 100, fill="yellow")
                 else:
-                    self.player = 'X'
+                    my_canvas.create_oval(100 * x, 100 * y, x * 100 + 100, y * 100 + 100, fill="red")
+                x += 1
+            y += 1
+            x = 0
 
-    def check_for_winner(self):
-        # Check horizontally by row
-        for i in range(self.board.get_num_rows()):
-            if (self.player * 4) in self.board.get_row(i):
-                return True
+        bottom_frame.pack(side="bottom")
+        my_canvas.pack()
 
-        # Check vertically by column
-        for i in range(self.board.get_num_columns()):
-            if (self.player * 4) in self.board.get_column(i):
-                return True
+        window.mainloop()
 
-        # Check diagonally every row first column up right and down right
-        for i in range(self.board.get_num_rows()):
-            if (self.player * 4) in self.board.get_diagonal(row=i, column=0, direction="dr"):
-                return True
-            if (self.player * 4) in self.board.get_diagonal(row=i, column=0, direction="ur"):
-                return True
-
-        # Check diagonally every column first row down right and every column last row up right
-        for i in range(self.board.get_num_columns()):
-            if (self.player * 4) in self.board.get_diagonal(row=0, column=i, direction="dr"):
-                return True
-            if (self.player * 4) in self.board.get_diagonal(row=self.board.get_num_rows() - 1, column=i, direction="ur"):
-                return True
-
-        return False
+        # print("Game Over")
+        # if self.player == 'T':
+        #     print("The game is a tie")
+        # elif self.player == "X":
+        #     print("The winner is player 1")
+        # else:
+        #     print("The winner is player 2")
--- Test.py
+++ Test.py
@@ -1,30 +1,96 @@
-from GameBoard import GameBoard
-
+# from GameBoard import GameBoard
+import tkinter
 
 def main():
-    test = GameBoard(rows=7, columns=6)
+    # test = GameBoard(rows=7, columns=6)
+    #
+    # test.print_board()
+    #
+    # print("Filling gameboard")
+    # letter = "a"
+    # for i in range(test.get_num_rows()):
+    #     for j in range(test.get_num_columns()):
+    #         test.set_position(letter, i, j)
+    #         letter = chr(ord(letter) + 1)
+    #         if letter == "{":
+    #             letter = "A"
+    #         # if letter == "Z":
+    #         #   letter = "a"
+    #
+    # print("Printing gameboard")
+    # test.print_board()
+    # print("Printing row 1")
+    # print(test.get_row(1))
+    # print("Printing column 1")
+    # print(test.get_column(1))
+    # print("Printing diagonal from 5, 2")
+    # print(test.get_diagonal(5, 2))
 
-    test.print_board()
+    from tkinter import Tk, Label, Button, StringVar
 
-    print("Filling gameboard")
-    letter = "a"
-    for i in range(test.get_num_rows()):
-        for j in range(test.get_num_columns()):
-            test.set_position(letter, i, j)
-            letter = chr(ord(letter) + 1)
-            if letter == "{":
-                letter = "A"
-            # if letter == "Z":
-            #   letter = "a"
 
-    print("Printing gameboard")
-    test.print_board()
-    print("Printing row 1")
-    print(test.get_row(1))
-    print("Printing column 1")
-    print(test.get_column(1))
-    print("Printing diagonal from 5, 2")
-    print(test.get_diagonal(5, 2))
+from tkinter import Tk, Label, Button, Entry, IntVar, END, W, E
+
+
+class Calculator:
+
+    def __init__(self, master):
+        self.master = master
+        master.title("Calculator")
+
+        self.total = 0
+        self.entered_number = 0
+
+        self.total_label_text = IntVar()
+        self.total_label_text.set(self.total)
+        self.total_label = Label(master, textvariable=self.total_label_text)
+
+        self.label = Label(master, text="Total:")
+
+        vcmd = master.register(self.validate) # we have to wrap the command
+        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
+
+        self.add_button = Button(master, text="+", command=lambda: self.update("add"))
+        self.subtract_button = Button(master, text="-", command=lambda: self.update("subtract"))
+        self.reset_button = Button(master, text="Reset", command=lambda: self.update("reset"))
+
+        # LAYOUT
+
+        self.label.grid(row=0, column=0, sticky=W)
+        self.total_label.grid(row=0, column=1, columnspan=2, sticky=E)
+
+        self.entry.grid(row=1, column=0, columnspan=3, sticky=W+E)
+
+        self.add_button.grid(row=2, column=0)
+        self.subtract_button.grid(row=2, column=1)
+        self.reset_button.grid(row=2, column=2, sticky=W+E)
+
+    def validate(self, new_text):
+        if not new_text: # the field is being cleared
+            self.entered_number = 0
+            return True
+
+        try:
+            self.entered_number = int(new_text)
+            return True
+        except ValueError:
+            return False
+
+    def update(self, method):
+        if method == "add":
+            self.total += self.entered_number
+        elif method == "subtract":
+            self.total -= self.entered_number
+        else: # reset
+            self.total = 0
+
+        self.total_label_text.set(self.total)
+        self.entry.delete(0, END)
+
+
+root = Tk()
+my_gui = Calculator(root)
+root.mainloop()
 
 
 if __name__ == "__main__":
